3f536ce0de9babea7977309b7ffe35a80f9801f4
Luca Lambda <luca@lambia.it>
2024-04-23 15:16:30 +0200

    aggiunta versione senza ciclo

---
+++ js/versioneDue.js
@@ -0,0 +1,21 @@
+//ad ogni click sul bottone, sposto la slide selezionata
+document.getElementById("btnNext").addEventListener("click", function () {
+
+    // recupero l'immagine attualmente selezionata
+    const immagineAttivaElement = document.querySelector('.slide.active');
+
+	// quella successiva è in omaggio
+    let immagineNextElement = immagineAttivaElement.nextElementSibling;
+
+    // se però siamo all'ultima immagine, non esiste nessuna "next", prendo la prima
+    if(immagineAttivaElement.classList.contains('last')) {
+        immagineNextElement = document.querySelector('.slide.first');
+    }
+
+    // immagini
+    immagineAttivaElement.classList.remove('active');
+    immagineNextElement.classList.add('active');
+
+	console.log(`Ho spostato la selezione`, immagineAttivaElement, immagineNextElement);
+
+});
